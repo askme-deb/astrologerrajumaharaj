4214667cbcada56cc6014e26de6813ac6a50f523
Debabrata <debabrata.mondal0@gmail.com>
2026-05-15 01:52:24 +0530

    Update products link in header to point to the main products page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -66,7 +66,7 @@
                 <li><a href="/about">About</a></li>
                 <li><a href="https://jyotish.astrorajumaharaj.com">Consultation</a></li>
                 <li><a href="#">Online Classes</a></li>
-                <li><a href="https://shop.astrorajumaharaj.com/products">Products</a></li>
+                <li><a href="https://shop.astrorajumaharaj.com">Products</a></li>
                 <li><a href="/gallery">Gallery</a></li>
                 <li><a href="/blog">Blog</a></li>
                 <li><a href="/contact">Contact</a></li>
